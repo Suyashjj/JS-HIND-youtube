a3ffaa0c0b0a4589accca24982cb4a9ba147b3fc
Suyashjj <suyashjai2010@gmail.com>
2024-08-09 13:01:39 +0000

    operations

---
+++ .devcontainer/01_basics/03_Operations.js
@@ -0,0 +1,31 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = "suyash"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+ 2);
+console.log(1+"2");
+console.log("1"+ 2 + 2); //starts with string behave like string
+console.log(1+2+"2");  //here first add operation perform then 2 act like string
+console.log((3+4)*5%3);
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+let gameCounter= 100
+++gameCounter;
+console.log(gameCounter);
+
+//increment and decrement
