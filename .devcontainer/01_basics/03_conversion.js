12be8f7e4d5e36b21fb266b27dc1e612fe72827e
Suyashjj <suyashjai2010@gmail.com>
2024-08-09 13:03:07 +0000

    conversions

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,18 @@
+//let score = 33
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn = "suyash"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);  //here number converted into string
+
